21dc47e2ea3982851ca0b73bed1683882dcbcdb6
Edsamn <eduardomsribas@gmail.comm>
2023-12-21 19:35:20 -0300

    commit

---
+++ ex1/scripts.js
@@ -0,0 +1,17 @@
+// 1. Criar um array e percorrê-lo utilizando o loop for
+// a. Crie um array com o nome "nomes" e adicione 4 nomes de
+// pessoas que você conhece
+// b. Utilize o loop for para percorrer o array e exibir os nomes na
+// tela
+
+let array = ['1','2','3','4','5'];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+let nomes = ['João', 'Maria', 'Pedro'];
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
